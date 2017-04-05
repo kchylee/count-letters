cf08493a251cb526f13936e7ab44db6a1ee10124
Ken Lee <kchylee@gmail.com>
2017-04-05 16:13:25 +0000

    Added for-loop and created object that produced expected results

--- count-letters.js
+++ count-letters.js
@@ -1,3 +1,17 @@
-function countLetters(){ //Takes string input
+function countLetters(inputStr){ //Takes string input
+//Return objects containing all unique characters as KEYS
+//and numbers of appearance as VALUE
+  var str = inputStr.split(" ").join("");
+  var uniqueLetters = {};
 
+  for (var i = 0; i < str.length; i++){
+    if (!uniqueLetters[str[i]]){
+      uniqueLetters[str[i]] = 1;
+    }else{
+      uniqueLetters[str[i]] += 1;
+    }
+  }
+  return uniqueLetters;
 }
+
+console.log(countLetters("lighthouse in the house"));
